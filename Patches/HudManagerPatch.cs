bf9ab5cebf516b3a141cfe9734824c094e6b46d1
Qlevine <Qlevine20@gmail.com>
2024-01-14 11:56:49 -0800

    Remove commented out code

--- Patches/HudManagerPatch.cs
+++ Patches/HudManagerPatch.cs
@@ -23,37 +23,6 @@ namespace DeadAndBored.Patches
                 }
             }
         }
-
-        //private static void CreateDescriptionText(HUDManager instance)
-        //{
-        //    TextMeshProUGUI endGameEarlyText = instance.holdButtonToEndGameEarlyText;
-        //    Transform textParent = endGameEarlyText.transform.GetParent();
-
-        //    GameObject newTextObj = new GameObject("DeadAndBoredText");
-        //    newTextObj.transform.parent = textParent;
-        //    TextMeshProUGUI newText = newTextObj.AddComponent<TextMeshProUGUI>();
-        //    RectTransform endGameEarlyTextTransform = endGameEarlyText.rectTransform;
-        //    RectTransform rectTransform = newTextObj.GetComponent<RectTransform>();
-        //    rectTransform.anchorMin = endGameEarlyTextTransform.anchorMin;
-        //    rectTransform.anchorMax = endGameEarlyTextTransform.anchorMax;
-        //    rectTransform.pivot = endGameEarlyTextTransform.pivot;
-        //    Debug.Log("DEAD AND BORED: Creating new description text");
-        //    Debug.Log($"DEAD AND BORED: Old Anchor = {endGameEarlyTextTransform.anchoredPosition}");
-        //    Debug.Log($"DEAD AND BORED: Old Anchor Min = {endGameEarlyTextTransform.anchorMin}");
-        //    Debug.Log($"DEAD AND BORED: Old Anchor Max = {endGameEarlyTextTransform.anchorMax}");
-        //    Debug.Log($"DEAD AND BORED: Old Font Size = {endGameEarlyText.fontSize}");
-        //    Debug.Log($"DEAD AND BORED: Old sizeDelta.y = {endGameEarlyTextTransform.sizeDelta.y}");
-        //    rectTransform.anchoredPosition = new Vector2(
-        //        endGameEarlyTextTransform.anchoredPosition.x,
-        //        endGameEarlyTextTransform.anchoredPosition.y - endGameEarlyTextTransform.sizeDelta.y - 20 //for padding
-        //        );
-        //    rectTransform.sizeDelta = new Vector2(300, 20);
-
-        //    newText.SetText($"Press {Config.deadAndTalkingKey.ToString()} to talk as enemy");
-        //    newText.color = Color.white;
-        //    newText.fontSize = endGameEarlyText.fontSize;
-        //    descriptionText = newText;
-        //}
     }
 
 }
